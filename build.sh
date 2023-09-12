13eb1beb072ac49b3959f43b175ed0cc7701f765
yakami <okapi0129@outlook.com>
2023-09-13 00:56:28 +0800

    feat: 解决docker镜像系统兼容问题

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker rmi okapi0129/virtualwife-chatbot
 docker rmi okapi0129/virtualwife-chatvrm
 docker rmi okapi0129/virtualwife-gateway
 docker buildx create --use
-docker buildx bake -f docker-compose.build.yaml --set *.platform=linux/amd64,linux/arm64/v8
+docker buildx bake -f docker-compose.build.yaml --set *.platform=linux/amd64,linux/arm64/v8 --push
