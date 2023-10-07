1919041ca3ff739ef2afc48e4ff96a9c22be7667
yakami <36467094+yakami129@users.noreply.github.com>
2023-10-07 10:23:48 +0800

    Revert "修改字幕为追加"

--- domain-chatvrm/src/pages/_app.tsx
+++ domain-chatvrm/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "@charcoal-ui/icons";
-import '@/fonts/font.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
